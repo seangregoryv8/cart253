ef5ab28fef01f8eba2809b09bcb76e2edc50e23d
Sean Gregory <seangregoryv8@gmail.com>
2025-09-24 17:55:06 -0400

    Part 1 done

--- assignments/challenges/conditionals/js/script.js
+++ assignments/challenges/conditionals/js/script.js
@@ -1,24 +1,73 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Circle Master
+ * Pippin Barr
+ *
+ * This will be a program in which the user can push a circle
+ * on the canvas using their own circle.
  */
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
-
-}
-
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
-}
+const puck = {
+    x: 200,
+    y: 200,
+    size: 100,
+    fill: "#ff0000"
+  };
+  
+  const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+  };
+  
+  /**
+   * Create the canvas
+   */
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  /**
+   * Move the user circle, check for overlap, draw the two circles
+   */
+  function draw() {
+    background("#aaaaaa");
+    
+    // Move user circle
+    moveUser();
+    
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+  }
+  
+  /**
+   * Sets the user position to the mouse position
+   */
+  function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+  }
+  
+  /**
+   * Displays the user circle
+   */
+  function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+  }
+  
+  /**
+   * Displays the puck circle
+   */
+  function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
+  }
+  
