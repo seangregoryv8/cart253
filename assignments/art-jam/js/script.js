e474ac19092613e9e3d3d8ef5d27feb1680dee18
Sean Gregory <chiefsgreg2622@gmail.com>
2025-09-29 10:23:21 -0400

    Started with the pixel art journey

--- assignments/art-jam/js/script.js
+++ assignments/art-jam/js/script.js
@@ -1,25 +1,56 @@
 /**
- * Title of Project
- * Author Name
+ * The Pixelated Artist
+ * Sean Gregory
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This will, for now, draw a pixelated face.
  */
-
+/// <reference path="path-to/p5.global-mode.d.ts" />
 "use strict";
 
+const WIDTH = 560;
+const HEIGHT = 680;
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * This sets up the grid in a 20x20 pixel art style.
 */
 function setup() {
-    createCanvas(1000, 1000);
-    background(0);
+    createCanvas(WIDTH, HEIGHT);
+    // 28x34 grid
+    background(220);
 }
 
+function drawGrid()
+{
+    fill(230);
+    // This is for the grid for the pixel art
+    // This is for the vertical lines at 34 pixels
+    for (let x = 0; x <= WIDTH; x += 20)
+    {
+        rect(x, 0, 20, 20);
+        // This is for the horizontal lines at 28 pixels
+        for (let y = 0; y <= HEIGHT; y += 20)
+            rect(x, y, 20, 20);
+    }
+}
+
+function drawFace()
+{
+    // This is for the face
+    fill(255, 224, 189);
+    rect(200, 140, 160, 160);
+    // This is for the eyes
+    fill(0);
+    rect(240, 180, 20, 20);
+    rect(300, 180, 20, 20);
+    // This is for the mouth
+    fill(255, 0, 0);
+    rect(250, 240, 60, 20);
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * This will be drawing the beautiful pixel art!
 */
 function draw() {
-
+    drawGrid();
+    drawFace();
 }
+
