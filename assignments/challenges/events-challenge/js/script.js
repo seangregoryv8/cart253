f63190628838a9f0a689a0b11769df318caa55e6
Sean Gregory <seangregoryv8@gmail.com>
2025-10-01 20:22:57 -0400

    Added the code and two of the steps

---
+++ assignments/challenges/events-challenge/js/script.js
@@ -0,0 +1,81 @@
+/**
+ * The Only Move Is Not To Play
+ * Pippin Barr
+ *
+ * A game where your score increases so long as you do nothing.
+ */
+
+"use strict";
+
+// Current score
+let score = 0;
+
+// Is the game over?
+let gameOver = false;
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Update the score and display the UI
+ */
+function draw() {
+  background("#87ceeb");
+  
+  // Only increase the score if the game is not over
+  if (!gameOver) {
+    // Score increases relatively slowly
+    score += 0.05;
+  }
+  displayUI();
+}
+
+/**
+ * Show the game over message if needed, and the current score
+ */
+function displayUI() {
+  if (gameOver) {
+    push();
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text("You lose!", width/2, height/3);
+    pop();
+  }
+  displayScore();
+}
+
+function keyPressed()
+{
+    lose();
+}
+
+function click()
+{
+    lose();
+}
+
+/**
+ * Display the score
+ */
+function displayScore() {
+  push();
+  textSize(48);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(floor(score), width/2, height/2);
+  pop();
+}
+
+function lose()
+{
+    gameOver = true;
+}
+
+document.getElementById("mainBody").addEventListener("click", () => { lose(); })
+document.getElementById("mainBody").addEventListener("mousemove", () => { lose(); })
+document.getElementById("mainBody").addEventListener("mousedown", () => { lose(); })
