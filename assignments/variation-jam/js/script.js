44c5e3070c4b335e33dd4b92f56ecaf5a6c740bf
Sean Gregory <seangregoryv8@gmail.com>
2025-11-26 12:45:58 -0500

    Added the assignment for what will eventually be breakout

---
+++ assignments/variation-jam/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+let WIDTH = 700;
+let HEIGHT = 1100;
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    background(0);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
